eb05862513cd9d53b1aa2a6b837e02f5f1aec1f6
KoL9mbLa <nikolai_lavrente@mail.ru>
2021-09-06 16:39:54 +0300

    Autotest

---
+++ src/test/java/AutoTest/TestRestTeamplate.java
@@ -0,0 +1,46 @@
+package AutoTest;
+
+
+import org.junit.Assert;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
+
+import java.lang.reflect.Type;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.ArrayList;
+import java.util.Set;
+import java.util.TreeSet;
+
+public class TestRestTeamplate {
+    @Test
+    public void testGetEmployeeListSuccess() {
+        RestTemplate restTemplate = new RestTemplate();
+        String str = "{\"clientId\":\"1337\",\"sex\":\"MALE\"}";
+        final String baseUrl = "http://localhost:8080/getClientSex?clientId=1337";
+        URI uri = null;
+        try {
+            uri = new URI(baseUrl);
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
+        }
+
+        ResponseEntity<String> result = restTemplate.getForEntity(uri, String.class);
+
+        //Verify request succeed
+        Assert.assertEquals(200, result.getStatusCodeValue());
+        Assert.assertEquals(str, result.getBody());
+
+    }
+}
+
+
+
---
+++ src/test/java/KafkaTest/KafkaProducerIn.java
@@ -0,0 +1,48 @@
+package KafkaTest;
+
+
+import org.apache.kafka.clients.producer.*;
+import org.apache.kafka.common.serialization.LongSerializer;
+import org.apache.kafka.common.serialization.StringSerializer;
+import java.util.Properties;
+
+public class KafkaProducerIn {
+
+    private final static String TOPIC = "in-topic-name";
+    private final static String BOOTSTRAP_SERVERS = "localhost:9092";
+
+    private static Producer<Long, String> createProducer() {
+        Properties props = new Properties();
+        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG,
+                BOOTSTRAP_SERVERS);
+        props.put(ProducerConfig.CLIENT_ID_CONFIG, "KafkaProducer");
+        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG,
+                LongSerializer.class.getName());
+        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG,
+                StringSerializer.class.getName());
+        return new KafkaProducer<>(props);
+}
+
+    static void runProducer(final int sendMessageCount) {
+         final Producer<Long, String> producer = createProducer();
+         long time = System.currentTimeMillis();
+
+                 for (long index = time; index < time + sendMessageCount; index++) {
+                         final ProducerRecord<Long, String> record =
+                                 new ProducerRecord<>(TOPIC, index,
+                                         "42");
+
+                         producer.send(record);
+                         System.out.printf("Record: key= "+ record.key() +" value= "+ record.value()+"\n");
+
+                        }
+                 producer.flush();
+                 producer.close();
+
+    }
+
+    public static void main(String[] args) throws Exception {
+        runProducer(5);
+    }
+}
+
